d8a26ca6400d17822c7e9ee5fa572e956347b5b4
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-02-28 19:40:13 -0300

    Improving visual messages

--- ui.rb
+++ ui.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 def da_boas_vindas
-  puts "/****************/"
-  puts "/ Jogo de Forca */"
-  puts "/****************/"
+  puts '/****************/'
+  puts '/ Jogo de Forca */'
+  puts '/****************/'
   puts 'Qual o seu nome?'
   nome = gets.strip
   puts "Olá #{nome}! Vamos começar o jogo!"
@@ -11,32 +11,32 @@ def da_boas_vindas
 end
 
 def desenha_forca(erros)
-  cabeca = "   "
-  corpo = " "
-  pernas = "   "
-  bracos = "   "
+  cabeca = '   '
+  corpo = ' '
+  pernas = '   '
+  bracos = '   '
   if erros >= 1
-    cabeca = "(_)"
+    cabeca = '(_)'
   end
   if erros >= 2
-    bracos = " | "
-    corpo = "|"
+    bracos = ' | '
+    corpo = '|'
   end
   if erros >= 3
-    bracos = "\\|/"
+    bracos = '\\|/'
   end
   if erros >= 4
-    pernas = "/ \\"
+    pernas = '/ \\'
   end
 
-  puts "  _______       "
-  puts " |/      |      "
+  puts '  _______       '
+  puts ' |/      |      '
   puts " |      #{cabeca}  "
   puts " |      #{bracos}  "
   puts " |       #{corpo}  "
   puts " |      #{pernas}  "
-  puts " |              "
-  puts "_|___           "
+  puts ' |              '
+  puts '_|___           '
   puts
 end
 
@@ -88,14 +88,14 @@ def avisa_acertou_palavra
   puts "\nParabéns, você ganhou!"
   puts
 
-  puts "       ___________      "
+  puts '       ___________      '
   puts "      '._==_==_=_.'     "
-  puts "      .-\\:      /-.    "
-  puts "     | (|:.     |) |    "
+  puts '      .-\\:      /-.    '
+  puts '     | (|:.     |) |    '
   puts "      '-|:.     |-'     "
-  puts "        \\::.    /      "
+  puts '        \\::.    /      '
   puts "         '::. .'        "
-  puts "           ) (          "
+  puts '           ) (          '
   puts "         _.' '._        "
   puts "        '-------'       "
   puts
